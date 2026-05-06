c3f090b6296e9091e00b2a037aef71a64dcbeadb
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-05-06 10:03:51 -0300

    feat: criação da abstração Usuario e implementação concreta
    - Define estrutura base do usuário
    - Implementa envio e recebimento de mensagens

---
+++ src/main/chat/Usuario.java
@@ -0,0 +1,15 @@
+package chat;
+
+public abstract class Usuario {
+
+    protected ChatMediator mediator;
+    protected String nome;
+
+    public Usuario(ChatMediator mediator, String nome) {
+        this.mediator = mediator;
+        this.nome = nome;
+    }
+
+    public abstract void enviar(String mensagem);
+    public abstract void receber(String mensagem);
+}
